996c44a00e6da4ffe40e38ae3e0bbb38bc7d9ee2
Ricardo Marques <ricardoalmmarques@gmail.com>
2018-04-07 22:14:16 -0500

    continue building execute spec

---
+++ src/PromiseMiddleware/__spec/index.spec.js
@@ -0,0 +1,22 @@
+import Promise from 'promise'
+import PromiseMiddleware from '../index'
+
+
+describe('PromiseMiddlware', () => {
+  const fetcher = () => new Promise(() => {})
+
+  let MyWrappedFetcher
+  beforeAll(() => {
+    MyWrappedFetcher = new PromiseMiddleware(fetcher)
+  })
+
+  describe('on construct', () => {
+    it('stores the given action', () => {
+      expect(MyWrappedFetcher._action).toEqual(fetcher)
+    })
+
+    it('initializes an empty middleware cache', () => {
+      expect(MyWrappedFetcher._middleware).toEqual({})
+    })
+  })
+})
